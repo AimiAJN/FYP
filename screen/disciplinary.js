089ad9d8609f2b7373c3073518b19c653b79c196
AimiAJN <43406106+AimiAJN@users.noreply.github.com>
2019-01-07 19:25:38 +0800

    Update disciplinary.js

--- screen/disciplinary.js
+++ screen/disciplinary.js
@@ -18,7 +18,8 @@ class disciplinaryScreen extends React.Component {
             <View style={styles.boxOne}>
                 <Icon name='ios-menu' style={{margin: 10}}/>
                 <AutoHeightImage width={150} style={{margin: 13}} source={{uri:'http://imaluum.iium.edu.my/assets/images/full-typeface.png'}}/>
-                <Icon name='ios-contact' style={{margin: 10}}/>            </View>
+                <Icon name='ios-contact' style={{margin: 10}}/>            
+            </View>
             <View style={styles.boxTwo}>
                 <Text style={{fontSize: 17, margin: 7}}>Disciplinary</Text>
             </View>
