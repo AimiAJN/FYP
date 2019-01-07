d4aea3c920e0557dd0299881fe18bc38360f5fb6
AimiAJN <43406106+AimiAJN@users.noreply.github.com>
2019-01-07 19:14:36 +0800

    Update myMahallah.js

--- screen/myMahallah.js
+++ screen/myMahallah.js
@@ -6,7 +6,8 @@ import AutoHeightImage from 'react-native-auto-height-image';
 class myMahallahScreen extends React.Component {
   static navigationOptions = {
     title: 'My Mahallah',
-    headerLeft: ( <Icon name="ios-menu" style={{margin: 10}}/>),
+    headerLeft: null,
+    //headerLeft: ( <Icon name="ios-menu" style={{margin: 10}}/>),
   };
     
   render() {
@@ -15,7 +16,9 @@ class myMahallahScreen extends React.Component {
     return(
         <View style={styles.container}>
             <View style={styles.boxOne}>
+                <Icon name='ios-menu' style={{margin: 10}}/>
                 <AutoHeightImage width={150} style={{margin: 13}} source={{uri:'http://imaluum.iium.edu.my/assets/images/full-typeface.png'}}/>
+                <Icon name='ios-contact' style={{margin: 10}}/>            
             </View>
             <View style={styles.boxTwo}>
                 <Text style={{fontSize: 17, margin: 7}}>My Mahallah</Text>
@@ -44,6 +47,8 @@ const styles = StyleSheet.create({
   boxOne: {
     alignItems: 'center', 
     backgroundColor: '#6ACA6B',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   boxTwo: {
     alignItems: 'center',
