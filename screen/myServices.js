c99245a3ebacce851b24a26a6159c518d07fccbd
AimiAJN <43406106+AimiAJN@users.noreply.github.com>
2019-01-07 15:34:51 +0800

    Update myServices.js

--- screen/myServices.js
+++ screen/myServices.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, ScrollView} from 'react-native';
+import {StyleSheet, Text, View, ScrollView, Linking} from 'react-native';
 import {Icon} from 'native-base';
 import AutoHeightImage from 'react-native-auto-height-image';
 
@@ -25,17 +25,17 @@ class myServicesScreen extends React.Component {
             <View style={{flexDirection: 'column', alignItems: 'center'}}>
                 <Text style={{margin: 5}}></Text>
                 <Text style={styles.list}>Finance</Text>
-                <Text style={styles.list}>iTa'leem</Text>
-                <Text style={styles.list}>SFS</Text>
-                <Text style={styles.list}>Library</Text>
-                <Text style={styles.list}>Mail</Text>
-                <Text style={styles.list}>Office 365 (Reset Password)</Text>
-                <Text style={styles.list}>Office 365 (Self Register)</Text>
-                <Text style={styles.list}>COP (Change of Programme)</Text>
-                <Text style={styles.list}>Intern</Text>
-                <Text style={styles.list}>SCS (Student Clearance System)</Text>
-                <Text style={styles.list}>i-Grad</Text>
-                <Text style={styles.list}>Counselling Session</Text>
+                <Text style={styles.list} onPress={ ()=> Linking.openURL('https://italeem.iium.edu.my/') }>iTa'leem</Text>
+                <Text style={styles.list} onPress={ ()=> Linking.openURL('http://sfs.iium.edu.my/sfs-cas/') }>SFS</Text>
+                <Text style={styles.list} onPress={ ()=> Linking.openURL('http://iium.ent.sirsidynix.net.au/client/en_GB/iiumlib/?dt=list') }>Library</Text>
+                <Text style={styles.list} onPress={ ()=> Linking.openURL('https://login.microsoftonline.com/common/oauth2/authorize?client_id=4345a7b9-9a63-4910-a426-35363201d503&response_mode=form_post&response_type=code+id_token&scope=openid+profile&state=OpenIdConnect.AuthenticationProperties%3dvvDHRUVDU9iUCe__RdT_kyHuhBJrTAMvN5s9yicNmyl0hF_lRptg03gvFeUfD5M36wM-gTwAhdMlPPsdZrd70qD2Gx2aoEp3Q7vNAIDNsAvHmD78EQufkg4F5ySCDgf0&nonce=636821323977884463.YzVmM2UyNWItY2U5ZC00ZGIxLTk2N2YtY2E3NDY0MDUzZjQ5OTRkMTNkOWItM2QyZi00NzMxLTlkN2ItMDUxMDkwYTY1ZWI3&redirect_uri=https%3a%2f%2fwww.office.com%2f&ui_locales=en-US&mkt=en-US') }>Mail</Text>
+                <Text style={styles.list} onPress={ ()=> Linking.openURL('') }>Office 365 (Reset Password)</Text>
+                <Text style={styles.list} onPress={ ()=> Linking.openURL('') }>Office 365 (Self Register)</Text>
+                <Text style={styles.list} onPress={ ()=> Linking.openURL('') }>COP (Change of Programme)</Text>
+                <Text style={styles.list} onPress={ ()=> Linking.openURL('') }>Intern</Text>
+                <Text style={styles.list} onPress={ ()=> Linking.openURL('') }>SCS (Student Clearance System)</Text>
+                <Text style={styles.list} onPress={ ()=> Linking.openURL('') }>i-Grad</Text>
+                <Text style={styles.list} onPress={ ()=> Linking.openURL('') }>Counselling Session</Text>
             </View>
             </ScrollView>
         </View> 
