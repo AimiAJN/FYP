ab8ce09a0cd56c4d884a25694a6b7c6435f2776a
AimiAJN <43406106+AimiAJN@users.noreply.github.com>
2019-01-08 01:57:01 +0800

    Update cocu.js

--- screen/cocu.js
+++ screen/cocu.js
@@ -16,7 +16,7 @@ class cocuScreen extends React.Component {
     return(
         <View style={styles.container}>
             <View style={styles.boxOne}>
-                <Icon name='ios-menu' style={{margin: 10}}/>
+                <Icon name='ios-menu' style={{margin: 10}} onPress={() => this.props.navigation.openDrawer()}/>
                 <AutoHeightImage width={150} style={{margin: 13}} source={{uri:'http://imaluum.iium.edu.my/assets/images/full-typeface.png'}}/>
                 <Icon name='ios-contact' style={{margin: 10}}/>            
             </View>
